251960d75d7199fc70960d460be373929047dc6e
Afolabi Rasheed <aforasheed385@gmail.com>
2023-03-07 23:04:45 +0100

    first commit

---
+++ src/context/shop-context.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const ShopContextProvider = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
---
+++ src/pages/shop/product.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const Product = (props) => {
+  const { id, productName, price, productImage } = props.data;
+  return <div className="product" key={id}>
+    <img src={productImage} alt='productImage'/>
+    <div className="description">
+        <p>
+            <b>{productName}</b>
+        </p>
+        <p>
+            ${price}
+        </p>
+    </div>
+    <button className="addToCartBttn">Add To Cart</button>
+  </div>;
+};
