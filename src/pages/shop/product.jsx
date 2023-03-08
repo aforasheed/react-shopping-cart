cd4ae107282a64cda83783c2c9199859931403a4
Afolabi Rasheed <aforasheed385@gmail.com>
2023-03-08 15:07:48 +0100

    completed

--- src/pages/shop/product.jsx
+++ src/pages/shop/product.jsx
@@ -1,17 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ShopContext } from "../../context/shop-context";
 
 export const Product = (props) => {
   const { id, productName, price, productImage } = props.data;
-  return <div className="product" key={id}>
-    <img src={productImage} alt='productImage'/>
-    <div className="description">
+  const { addToCart, cartItems } = useContext(ShopContext);
+
+  const cartItemAmount = cartItems[id];
+  return (
+    <div className="product" key={id}>
+      <img src={productImage} alt="productImage" />
+      <div className="description">
         <p>
-            <b>{productName}</b>
+          <b>{productName}</b>
         </p>
-        <p>
-            ${price}
-        </p>
+        <p>${price}</p>
+      </div>
+      <button className="addToCartBttn" onClick={() => addToCart(id)}>
+        Add To Cart {cartItemAmount > 0 && <>({cartItemAmount})</>}
+      </button>
     </div>
-    <button className="addToCartBttn">Add To Cart</button>
-  </div>;
+  );
 };
